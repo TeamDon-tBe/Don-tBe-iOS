9e3e5b2e566eb0e9dfbb6e010771fa5cdb014721
heejoo <heejoo_byun@naver.com>
2024-01-06 16:48:50 +0900

    [Feat] #8 - UIFont+ Extension 추가

---
+++ DontBe-iOS/DontBe-iOS/Global/Extension/UIFont+.swift
@@ -0,0 +1,39 @@
+//
+//  UIFont+.swift
+//  DontBe-iOS
+//
+//  Created by 변희주 on 1/6/24.
+//
+
+import UIKit
+
+enum FontName: String {
+    case head1, head2, head3
+    case body1, body2, body3,body4
+    case caption1, caption2, caption3, caption4
+
+    var rawValue: String {
+        switch self {
+        case .head1, .head2, .head3, .body1, .body3, .caption1, .caption3: return "Pretendard-SemiBold"
+        case .body2, .body4, .caption2, .caption4: return "Pretendard-Regular"
+        }
+    }
+
+    var size: CGFloat {
+        switch self {
+        case .head1: return 24
+        case .head2: return 20
+        case .head3: return 18
+        case .body1, .body2: return 16
+        case .body3, .body4: return 14
+        case .caption1, .caption2: return 13
+        case .caption3, .caption4: return 12
+        }
+    }
+}
+
+extension UIFont {
+    static func font(_ style: FontName) -> UIFont {
+        return UIFont(name: style.rawValue, size: style.size)!
+    }
+}
