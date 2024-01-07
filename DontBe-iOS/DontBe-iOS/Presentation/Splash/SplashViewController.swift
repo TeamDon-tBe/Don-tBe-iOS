c30c05f8a31524b2bc01ec640b5c090b53ac4546
heejoo <heejoo_byun@naver.com>
2024-01-08 01:26:25 +0900

    [Feat] #16 - 스플래시 화면 구현

---
+++ DontBe-iOS/DontBe-iOS/Presentation/Splash/SplashViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SplashViewController.swift
+//  DontBe-iOS
+//
+//  Created by 변희주 on 1/7/24.
+//
+
+import UIKit
+
+import SnapKit
+
+final class SplashViewController: UIViewController {
+
+    private let dontBeLogo: UIImageView = {
+       let logo = UIImageView()
+        logo.image = ImageLiterals.Home.textLogo
+        return logo
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        setLayout()
+    }
+    
+    private func setUI() {
+        view.backgroundColor = .white
+    }
+    
+    private func setLayout() {
+        self.view.addSubview(dontBeLogo)
+        dontBeLogo.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.equalTo(230.adjusted)
+            $0.height.equalTo(35.adjustedH)
+        }
+    }
+
+}
