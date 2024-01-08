72a8cf6b83ecb383d3170b0214a367f3e9cb2d9d
heejoo <heejoo_byun@naver.com>
2024-01-08 23:25:01 +0900

    [Feat] #18 - login viewmodel 구현

--- DontBe-iOS/DontBe-iOS/Presentation/Login/ViewModels/LoginViewModel.swift
+++ DontBe-iOS/DontBe-iOS/Presentation/Login/ViewModels/LoginViewModel.swift
@@ -5,4 +5,53 @@
 //  Created by 변희주 on 1/8/24.
 //
 
+import Combine
 import Foundation
+
+import KakaoSDKAuth
+import KakaoSDKCommon
+import KakaoSDKUser
+
+final class LoginViewModel: ViewModelType {
+    private let cancelBag = CancelBag()
+    
+    struct Input {
+        let kakaoButtonTapped: AnyPublisher<Void, Never>
+    }
+    
+    struct Output {
+        let userInfoPublisher: PassthroughSubject<String, Never>
+    }
+    
+    func transform(from input: Input, cancelBag: CancelBag) -> Output {
+        let userInfoPublisher = PassthroughSubject<String, Never>()
+        
+        input.kakaoButtonTapped
+            .sink { _ in
+                if (UserApi.isKakaoTalkLoginAvailable()) {
+                    UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                        if let error {
+                            print(error)
+                        } else {
+                            if let accessToken = oauthToken?.accessToken {
+                                userInfoPublisher.send(accessToken)
+                            }
+                        }
+                    }
+                } else {
+                    UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                        if let error {
+                            print(error)
+                        } else {
+                            if let accessToken = oauthToken?.accessToken {
+                                userInfoPublisher.send(accessToken)
+                            }
+                        }
+                    }
+                }
+            }
+            .store(in: self.cancelBag)
+        
+        return Output(userInfoPublisher: userInfoPublisher)
+    }
+}
