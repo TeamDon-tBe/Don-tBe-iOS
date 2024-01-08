babc9bd6879491ecfea26809bd60348424ce6007
heejoo <heejoo_byun@naver.com>
2024-01-08 19:36:22 +0900

    [Feat] #18 - LoginTitle 적용

--- DontBe-iOS/DontBe-iOS/Global/Literals/StringLiterals.swift
+++ DontBe-iOS/DontBe-iOS/Global/Literals/StringLiterals.swift
@@ -5,7 +5,7 @@
 //  Created by 변상우 on 12/26/23.
 //
 
-import UIKit
+import Foundation
 
 enum StringLiterals {
     enum Tabbar {
@@ -13,5 +13,9 @@ enum StringLiterals {
         static let writing = "글쓰기"
         static let notification = "알림"
         static let myPage = "마이"
+    }
+    
+    enum Login {
+        static let title = "온화한 커뮤니티\nDON’T BE에서 만나요."
     }
 }
--- DontBe-iOS/DontBe-iOS/Presentation/Login/LoginViewController.swift
+++ DontBe-iOS/DontBe-iOS/Presentation/Login/LoginViewController.swift
@@ -16,7 +16,24 @@ final class LoginViewController: UIViewController {
     private let loginLogo: UIImageView = {
         let logo = UIImageView()
         logo.image = ImageLiterals.Login.icnLogo
+        logo.contentMode = .scaleAspectFill
+        logo.clipsToBounds = true
         return logo
+    }()
+    
+    private let loginTitle: UILabel = {
+        let title = UILabel()
+        title.text = StringLiterals.Login.title
+        title.textColor = .black
+        title.numberOfLines = 2
+        title.font = .font(.head1)
+        return title
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.setImage(ImageLiterals.Login.btnKakao, for: .normal)
+        return button
     }()
     
     // MARK: - Life Cycles
@@ -38,7 +55,9 @@ extension LoginViewController {
     }
     
     private func setHierarchy() {
-        self.view.addSubview(loginLogo)
+        self.view.addSubviews(loginLogo,
+                              loginTitle,
+                              loginButton)
 
     }
     
@@ -50,9 +69,15 @@ extension LoginViewController {
             .statusBarFrame.height ?? 20
         
         loginLogo.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(statusBarHeight + 41.adjusted)
-            $0.leading.equalToSuperview().inset(27.adjusted)
-            $0.size.equalTo(78.adjusted)
+            $0.top.equalToSuperview().inset(statusBarHeight + 65.adjustedH)
+            $0.leading.equalToSuperview().inset(20.adjusted)
+            $0.width.equalTo(88.adjusted)
+            $0.height.equalTo(88.adjustedH)
+        }
+        
+        loginTitle.snp.makeConstraints {
+            $0.top.equalTo(loginLogo.snp.bottom).offset(20.adjustedH)
+            $0.leading.equalToSuperview().inset(26.adjusted)
         }
     }
 }
