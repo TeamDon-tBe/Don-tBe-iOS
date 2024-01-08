45a0e4ae4d463b0f3051d2c3002bd087496b6230
heejoo <heejoo_byun@naver.com>
2024-01-08 23:17:40 +0900

    [Feat] #18 - UIControl Extension 추가

--- DontBe-iOS/DontBe-iOS/Global/Extension/Publisher+UIControl.swift
+++ DontBe-iOS/DontBe-iOS/Global/Extension/Publisher+UIControl.swift
@@ -5,4 +5,59 @@
 //  Created by 변희주 on 1/8/24.
 //
 
-import Foundation
+import Combine
+import UIKit
+
+public protocol CombineCompatible { }
+
+extension UIControl: CombineCompatible { }
+
+public extension CombineCompatible where Self: UIControl {
+    func publisher(for events: UIControl.Event) -> UIControlPublisher<Self> {
+        return UIControlPublisher(control: self, events: events)
+    }
+}
+
+final class UIControlSubscription<SubscriberType: Subscriber, Control: UIControl>: Subscription where SubscriberType.Input == Control {
+    private var subscriber: SubscriberType?
+    private let control: Control
+
+    init(subscriber: SubscriberType, control: Control, event: UIControl.Event) {
+        self.subscriber = subscriber
+        self.control = control
+        control.addTarget(self, action: #selector(eventHandler), for: event)
+    }
+
+    func request(_ demand: Subscribers.Demand) {
+        
+    }
+
+    func cancel() {
+        subscriber = nil
+    }
+
+    @objc private func eventHandler() {
+        _ = subscriber?.receive(control)
+    }
+}
+
+public struct UIControlPublisher<Control: UIControl>: Publisher {
+
+    public typealias Output = Control
+    public typealias Failure = Never
+
+    let control: Control
+    let controlEvents: UIControl.Event
+
+    init(control: Control, events: UIControl.Event) {
+        self.control = control
+        self.controlEvents = events
+    }
+    
+    public func receive<S>(subscriber: S) where S: Subscriber,
+                                                S.Failure == UIControlPublisher.Failure,
+                                                S.Input == UIControlPublisher.Output {
+        let subscription = UIControlSubscription(subscriber: subscriber, control: control, event: controlEvents)
+        subscriber.receive(subscription: subscription)
+    }
+}
