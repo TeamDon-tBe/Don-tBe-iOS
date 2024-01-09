aaa93ed8c27a66e0c18f987f81fd4329c8a66c43
Sangwoo Byeon <tkddn0518@naver.com>
2024-01-10 01:38:43 +0900

    [Feat] #13 - 글쓰기 뷰 UI 구현

---
+++ DontBe-iOS/DontBe-iOS/Presentation/Write/Views/WriteTextView.swift
@@ -0,0 +1,166 @@
+//
+//  WriteTextView.swift
+//  DontBe-iOS
+//
+//  Created by 변상우 on 1/8/24.
+//
+
+import UIKit
+
+import SnapKit
+
+final class WriteTextView: UIView {
+
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    
+    private let userProfileImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.setCircularImage(image: ImageLiterals.Common.logoSymbol)
+        return imageView
+    }()
+    
+    private let userNickname: UILabel = {
+        let label = UILabel()
+        label.text = "뚜비요"
+        label.font = UIFont.font(.body1)
+        label.textColor = .donBlack
+        return label
+    }()
+    
+    let contentTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont.font(.body4)
+        textView.textColor = .donBlack
+        textView.tintColor = .donPrimary
+        textView.backgroundColor = .clear
+        textView.addPlaceholder(StringLiterals.Write.writeContentPlaceholder)
+        
+        textView.textContainerInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        textView.textContainer.lineFragmentPadding = 0
+        textView.textContainer.lineBreakMode = .byWordWrapping
+        textView.textContainer.maximumNumberOfLines = 0
+        return textView
+    }()
+    
+    private let keyboardToolbarView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .donWhite
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.donGray2.cgColor
+        return view
+    }()
+    
+    private let circleProgressBar: CircleProgressbar = {
+        let circle = CircleProgressbar()
+        circle.backgroundColor = .clear
+        circle.circleTintColor = .donPrimary
+        circle.circleBackgroundColor = .donGray3
+        return circle
+    }()
+    
+    private let limitedCircleProgressBar: CircleProgressbar = {
+        let circle = CircleProgressbar()
+        circle.backgroundColor = .clear
+        circle.value = 1.0
+        circle.circleTintColor = .donError
+        circle.circleBackgroundColor = .donError
+        return circle
+    }()
+    
+    let postButton: UIButton = {
+        let button = UIButton()
+        button.setTitle(StringLiterals.Write.writePostButtonTitle, for: .normal)
+        button.setTitleColor(.donGray9, for: .normal)
+        button.titleLabel?.font = UIFont.font(.body3)
+        button.backgroundColor = .donGray3
+        button.layer.cornerRadius = 4.adjusted
+        return button
+    }()
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setDelegate()
+        setUI()
+        setHierarchy()
+        setLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Extensions
+
+extension WriteTextView {
+    func setDelegate() {
+    }
+    
+    func setUI() {
+        contentTextView.becomeFirstResponder()
+        limitedCircleProgressBar.alpha = 0
+        
+    }
+    
+    func setHierarchy() {
+        self.addSubviews(userProfileImage, 
+                         userNickname,
+                         contentTextView,
+                         keyboardToolbarView)
+        
+        keyboardToolbarView.addSubviews(circleProgressBar,
+                                        limitedCircleProgressBar,
+                                        postButton)
+    }
+    
+    func setLayout() {
+        userProfileImage.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide).offset(12.adjusted)
+            $0.leading.equalToSuperview().inset(16.adjusted)
+            $0.width.equalTo(44.adjusted)
+            $0.height.equalTo(44.adjusted)
+        }
+        
+        userNickname.snp.makeConstraints {
+            $0.top.equalTo(userProfileImage.snp.top).offset(1.adjusted)
+            $0.leading.equalTo(userProfileImage.snp.trailing).offset(11.adjusted)
+        }
+        
+        contentTextView.snp.makeConstraints {
+            $0.top.equalTo(userNickname.snp.bottom).offset(4.adjusted)
+            $0.leading.equalTo(userNickname.snp.leading)
+            $0.trailing.equalToSuperview().inset(16.adjusted)
+            $0.bottom.equalToSuperview()
+        }
+        
+        keyboardToolbarView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(56.adjusted)
+            $0.bottom.equalToSuperview()
+        }
+        
+        circleProgressBar.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(84.adjusted)
+            $0.width.height.equalTo(20.adjusted)
+        }
+        
+        limitedCircleProgressBar.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(84.adjusted)
+            $0.width.height.equalTo(20.adjusted)
+        }
+        
+        postButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(16.adjusted)
+            $0.width.equalTo(60.adjusted)
+            $0.height.equalTo(36.adjusted)
+        }
+    }
