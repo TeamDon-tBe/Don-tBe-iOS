ec919ce0928c7692ed68faed7be69c94c25541d6
Sangwoo Byeon <tkddn0518@naver.com>
2024-01-10 02:00:43 +0900

    [Add] #13 - 글쓰기에 쓰이는 String, Image, UIcolor 값 새로 추가

--- DontBe-iOS/DontBe-iOS/Global/Literals/StringLiterals.swift
+++ DontBe-iOS/DontBe-iOS/Global/Literals/StringLiterals.swift
@@ -14,4 +14,14 @@ enum StringLiterals {
         static let notification = "알림"
         static let myPage = "마이"
     }
+    
+    enum Write {
+        static let writeNavigationTitle = "새로운 글"
+        static let writeNavigationBarButtonItemTitle = "취소"
+        static let writeContentPlaceholder = "어떤 생각을 하고 있나요?"
+        static let writePostButtonTitle = "게시"
+        static let writePopupContentLabel = "작성 중인 글을 삭제하시겠어요?"
+        static let writePopupCancleButtonTitle = "취소"
+        static let writePopupConfirmButtonTitle = "삭제"
+    }
 }
