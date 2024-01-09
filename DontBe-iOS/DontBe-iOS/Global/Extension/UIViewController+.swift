06dc2081211d78f6f2f27491ac7fd4d170c982a8
heejoo <heejoo_byun@naver.com>
2024-01-10 02:40:23 +0900

    [Feat] #18 - 지피티 코드리뷰 반영

--- DontBe-iOS/DontBe-iOS/Global/Extension/UIViewController+.swift
+++ DontBe-iOS/DontBe-iOS/Global/Extension/UIViewController+.swift
@@ -9,10 +9,6 @@ import UIKit
 
 extension UIViewController {
     var statusBarHeight: CGFloat {
-        return UIApplication.shared.connectedScenes
-            .compactMap { $0 as? UIWindowScene }
-            .first?
-            .statusBarManager?
-            .statusBarFrame.height ?? 20
+        return UIApplication.shared.windows.filter({$0.isKeyWindow}).first?.windowScene?.statusBarManager?.statusBarFrame.height ?? 20
     }
 }
