74d92160a8886253b195292e05cbdf15dfb7f626
Sangwoo Byeon <tkddn0518@naver.com>
2024-01-11 00:21:46 +0900

    [Add] #27 - MyPageViewController, MyPageView 파일 생성

---
+++ DontBe-iOS/DontBe-iOS/Presentation/MyPage/Views/MyPageView.swift
@@ -0,0 +1,20 @@
+//
+//  MyPageView.swift
+//  DontBe-iOS
+//
+//  Created by 변상우 on 1/11/24.
+//
+
+import UIKit
+
+class MyPageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
