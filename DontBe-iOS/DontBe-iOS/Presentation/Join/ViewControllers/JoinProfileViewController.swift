07ae420c448483cdd07a461b9b680a70313b61b6
heejoo <heejoo_byun@naver.com>
2024-01-11 03:54:06 +0900

    [Feat] #31 - 텍스트 필드 한글 글자수 제한

--- DontBe-iOS/DontBe-iOS/Presentation/Join/ViewControllers/JoinProfileViewController.swift
+++ DontBe-iOS/DontBe-iOS/Presentation/Join/ViewControllers/JoinProfileViewController.swift
@@ -17,7 +17,7 @@ final class JoinProfileViewController: UIViewController {
     private let viewModel: JoinProfileViewModel
     
     private lazy var backButtonTapped = navigationBackButton.publisher(for: .touchUpInside).map { _ in }.eraseToAnyPublisher()
-        
+    
     // MARK: - UI Components
     
     private let navigationBackButton = BackButton()
@@ -48,6 +48,9 @@ final class JoinProfileViewController: UIViewController {
         super.viewDidLoad()
         
         setUI()
+        setHierarchy()
+        setLayout()
+        setDelegate()
         bindViewModel()
     }
     
@@ -56,8 +59,6 @@ final class JoinProfileViewController: UIViewController {
         
         self.navigationController?.navigationBar.isHidden = false
         self.navigationItem.hidesBackButton = true
-        setHierarchy()
-        setLayout()
     }
 }
 
@@ -81,6 +82,10 @@ extension JoinProfileViewController {
         }
     }
     
+    private func setDelegate() {
+        self.originView.nickNameTextField.delegate = self
+    }
+    
     private func bindViewModel() {
         let input = JoinProfileViewModel.Input(backButtonTapped: backButtonTapped)
         let output = self.viewModel.transform(from: input, cancelBag: self.cancelBag)
@@ -90,6 +95,49 @@ extension JoinProfileViewController {
                 self.navigationController?.popViewController(animated: true)
             }
             .store(in: self.cancelBag)
+    }
+}
+
+// MARK: - UITextFieldDelegate
+
+extension JoinProfileViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let maxLength = 12 // 글자수 제한
+        let oldText = textField.text ?? "" // 입력하기 전 textField에 표시되어있던 text
+        let addedText = string // 입력한 text
+        let newText = oldText + addedText // 입력하기 전 text와 입력한 후 text를 합침
+        let newTextLength = newText.count // 합쳐진 text의 길이
         
+        // 글자수 제한
+        if newTextLength <= maxLength {
+            return true
+        }
+        
+        let lastWordOfOldText = String(oldText[oldText.index(before: oldText.endIndex)]) // 입력하기 전 text의 마지막 글자
+        let separatedCharacters = lastWordOfOldText.decomposedStringWithCanonicalMapping.unicodeScalars.map{ String($0) } // 입력하기 전 text의 마지막 글자를 자음과 모음으로 분리
+        let separatedCharactersCount = separatedCharacters.count // 분리된 자음, 모음의 개수
+        
+        if separatedCharactersCount == 1 && !addedText.isConsonant {
+            return true
+        } else if separatedCharactersCount == 2 && addedText.isConsonant {
+            return true
+        } else if separatedCharactersCount == 3 && addedText.isConsonant {
+            return true
+        }
+        return false
+    }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        var text = textField.text ?? "" // textField에 수정이 반영된 후의 text
+        let maxLength = 12 // 글자 수 제한
+        if text.count >= maxLength {
+            let startIndex = text.startIndex
+            let endIndex = text.index(startIndex, offsetBy: maxLength - 1)
+            let fixedText = String(text[startIndex...endIndex])
+            textField.text = fixedText
+            self.originView.numOfLetters.text = "12/12"
+        } else {
+            self.originView.numOfLetters.text = "\(text.count)/12"
+        }
     }
 }
