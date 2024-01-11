02dd2685e28063c3ed981bd3fdac0fe397eb6889
Sangwoo Byeon <tkddn0518@naver.com>
2024-01-11 16:10:56 +0900

    [Feat] #27 - 마이페이지에서 글쓰기뷰, 댓글 뷰 SegmentControl 에 연결

---
+++ DontBe-iOS/DontBe-iOS/Presentation/MyPage/ViewControllers/MyPageContentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyPageContentViewController.swift
+//  DontBe-iOS
+//
+//  Created by 변상우 on 1/11/24.
+//
+
+import UIKit
+
+class MyPageContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .donWhite
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- DontBe-iOS/DontBe-iOS/Presentation/MyPage/ViewControllers/MyPageViewController.swift
+++ DontBe-iOS/DontBe-iOS/Presentation/MyPage/ViewControllers/MyPageViewController.swift
@@ -11,6 +11,18 @@ final class MyPageViewController: UIViewController {
     
     // MARK: - Properties
     
+    var currentPage: Int = 0 {
+        didSet {
+            let direction: UIPageViewController.NavigationDirection = oldValue <= self.currentPage ? .forward : .reverse
+            rootView.pageViewController.setViewControllers(
+                [rootView.dataViewControllers[self.currentPage]],
+                direction: direction,
+                animated: true,
+                completion: nil
+            )
+        }
+    }
+    
     // MARK: - UI Components
     
     private let rootView = MyPageView()
@@ -36,15 +48,31 @@ final class MyPageViewController: UIViewController {
 
 extension MyPageViewController {
     private func setUI() {
+        self.title = StringLiterals.MyPage.MyPageNavigationTitle
+        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.donWhite]
         
+        let image = ImageLiterals.MyPage.icnMenu
+        let renderedImage = image.withRenderingMode(.alwaysOriginal)
+        let hambergerButton = UIBarButtonItem(image: renderedImage,
+                                           style: .plain,
+                                           target: self,
+                                           action: nil)
+        
+        navigationItem.rightBarButtonItem = hambergerButton
     }
     
     private func setDelegate() {
-        
+        rootView.pageViewController.delegate = self
+        rootView.pageViewController.dataSource = self
     }
     
     private func setAddTarget() {
-        
+        rootView.segmentedControl.addTarget(self, action: #selector(changeValue(control:)), for: .valueChanged)
+    }
+    
+    @objc private func changeValue(control: UISegmentedControl) {
+        // 코드로 값을 변경하면 해당 메소드 호출 x
+        self.currentPage = control.selectedSegmentIndex
     }
 }
 
@@ -53,5 +81,43 @@ extension MyPageViewController {
 extension MyPageViewController {
     private func getAPI() {
         
+    }
+}
+
+extension MyPageViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        viewControllerBefore viewController: UIViewController
+    ) -> UIViewController? {
+        guard
+            let index = rootView.dataViewControllers.firstIndex(of: viewController),
+            index - 1 >= 0
+        else { return nil }
+        return rootView.dataViewControllers[index - 1]
+    }
+    
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        viewControllerAfter viewController: UIViewController
+    ) -> UIViewController? {
+        guard
+            let index = rootView.dataViewControllers.firstIndex(of: viewController),
+            index + 1 < rootView.dataViewControllers.count
+        else { return nil }
+        return rootView.dataViewControllers[index + 1]
+    }
+    
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        didFinishAnimating finished: Bool,
+        previousViewControllers: [UIViewController],
+        transitionCompleted completed: Bool
+    ) {
+        guard
+            let viewController = pageViewController.viewControllers?[0],
+            let index = rootView.dataViewControllers.firstIndex(of: viewController)
+        else { return }
+        self.currentPage = index
+        rootView.segmentedControl.selectedSegmentIndex = index
     }
 }
