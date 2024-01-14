42d7d1945dff30d999238a769f92d485c6061d87
Sangwoo Byeon <tkddn0518@naver.com>
2024-01-15 04:31:12 +0900

    [Network] #63 - 마이페이지 계정 정보 ResponseDTO 생성

---
+++ DontBe-iOS/DontBe-iOS/Network/MyPage/DTO/MyPageMemberDataResponseDTO.swift
@@ -0,0 +1,17 @@
+//
+//  MyPageMemberDataResponseDTO.swift
+//  DontBe-iOS
+//
+//  Created by 변상우 on 1/14/24.
+//
+
+import Foundation
+
+// MARK: - MyPageMemberDataResponseDTO
+struct MyPageMemberDataResponseDTO: Decodable {
+    let memberId: Int
+    let joinDate: String
+    let showMemberId: String?
+    let socialPlatform: String
+    let versionInformation: String
+}
