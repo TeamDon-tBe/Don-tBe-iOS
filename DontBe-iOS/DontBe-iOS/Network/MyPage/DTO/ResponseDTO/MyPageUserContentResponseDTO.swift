6be914124f090e062512f32ee6ae5ed0cf83eb66
Sangwoo Byeon <tkddn0518@naver.com>
2024-01-17 05:05:10 +0900

    [Network] #80 - 유저에 해당하는 게시글 리스트 조회 API ResponseDTO 생성

---
+++ DontBe-iOS/DontBe-iOS/Network/MyPage/DTO/ResponseDTO/MyPageUserContentResponseDTO.swift
@@ -0,0 +1,24 @@
+//
+//  MyPageUserContentResponseDTO.swift
+//  DontBe-iOS
+//
+//  Created by 변상우 on 1/17/24.
+//
+
+import Foundation
+
+// MARK: - MyPageUserContentResponseDTO
+
+struct MyPageUserContentResponseDTO: Decodable {
+    let memberId: Int
+    let memberProfileUrl: String
+    let memberNickname: String
+    let contentId: Int
+    let contentText: String
+    let time: String
+    let isGhost: Bool
+    let memberGhost: Int
+    let isLiked: Bool
+    let likedNumber: Int
+    let commentNumber: Int
+}
