626088aaad46219246cf35a39fe96d880aba2323
yeonsu <drawingbonny@gmail.com>
2024-01-16 14:42:25 +0900

    [Feat] #73 - 게시글 리스트 조회 ResponseDTO 생성

---
+++ DontBe-iOS/DontBe-iOS/Network/Home/DTO/PostDataResponseDTO.swift
@@ -0,0 +1,22 @@
+//
+//  PostDataResponseDTO.swift
+//  DontBe-iOS
+//
+//  Created by yeonsu on 1/16/24.
+//
+
+import Foundation
+
+struct PostDataResponseDTO: Decodable {
+    let memberId: Int
+    let memberProfileUrl: String
+    let memberNickname: String
+    let contentId: Int
+    let contentText: String
+    let time: String
+    let isGhost: Bool
+    let memberGhost: Int
+    let isLiked: Bool
+    let likedNumber: Int
+    let commentNumber: Int
+}
