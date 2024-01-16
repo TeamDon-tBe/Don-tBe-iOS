7149ec6733239af0bc3da8b801958795253c832f
yeonsu <drawingbonny@gmail.com>
2024-01-16 17:54:46 +0900

    [Feat] #73 - 홈 게시글 최신순으로 정렬

--- DontBe-iOS/DontBe-iOS/Presentation/Home/ViewControllers/HomeViewController.swift
+++ DontBe-iOS/DontBe-iOS/Presentation/Home/ViewControllers/HomeViewController.swift
@@ -226,6 +226,12 @@ extension HomeViewController: UICollectionViewDelegate { }
 
 extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let sortedData = viewModel.postData.sorted {
+                $0.time.compare($1.time, options: .numeric) == .orderedDescending
+            }
+            
+            // Replace the viewModel.postData array with the sortedData
+            viewModel.postData = sortedData
         
         return viewModel.postData.count
     }
