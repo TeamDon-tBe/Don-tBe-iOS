92e1602ba3f975e91b67260651b7adfd401a2501
yeonsu <drawingbonny@gmail.com>
2024-01-18 06:57:00 +0900

    [Network] #92 - 게시글 삭제 ViewModel 생성

---
+++ DontBe-iOS/DontBe-iOS/Presentation/Post/ViewModel/DeletePostViewModel.swift
@@ -0,0 +1,68 @@
+//
+//  DeletePostViewModel.swift
+//  DontBe-iOS
+//
+//  Created by yeonsu on 1/18/24.
+//
+
+import Foundation
+import Combine
+
+final class DeletePostViewModel: ViewModelType {
+    
+    private let cancelBag = CancelBag()
+    private let networkProvider: NetworkServiceType
+    private let popView = PassthroughSubject<Void, Never>()
+    
+    struct Input {
+        let deleteButtonDidTapped: AnyPublisher<Int, Never>
+    }
+    
+    struct Output {
+        let popView: PassthroughSubject<Void, Never>
+        
+    }
+    
+    func transform(from input: Input, cancelBag: CancelBag) -> Output {
+        input.deleteButtonDidTapped
+            .sink { value in
+                Task {
+                    do {
+                        if let accessToken = KeychainWrapper.loadToken(forKey: "accessToken") {
+                            let statusCode = try await self.deletePostAPI(accessToken: accessToken, contentId: value)?.status
+                            if statusCode == 200 {
+                                self.popView.send()
+                            }
+                        }
+                    } catch {
+                        print(error)
+                    }
+                }
+            }
+            .store(in: self.cancelBag)
+        return Output(popView: popView)
+    }
+    
+    init(networkProvider: NetworkServiceType) {
+        self.networkProvider = networkProvider
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension DeletePostViewModel {
+    private func deletePostAPI(accessToken: String, contentId: Int) async throws -> BaseResponse<EmptyResponse>? {
+        let accessToken = accessToken
+        do {
+            let result: BaseResponse<EmptyResponse>? = try
+            await self.networkProvider.donNetwork(type: .delete, baseURL: Config.baseURL + "/content/\(contentId)", accessToken: accessToken, body: EmptyBody(), pathVariables: ["":""])
+            
+            return result
+        } catch {
+            return nil
+        }
+    }
+}
+
