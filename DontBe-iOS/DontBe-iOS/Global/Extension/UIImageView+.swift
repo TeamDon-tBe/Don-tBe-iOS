a903983c73a23d0bb8b97032eec773213f2b4c0a
Sangwoo Byeon <tkddn0518@naver.com>
2024-01-18 00:04:18 +0900

    [Fix] #84 - 이미지 잘리는 오류 해결

--- DontBe-iOS/DontBe-iOS/Global/Extension/UIImageView+.swift
+++ DontBe-iOS/DontBe-iOS/Global/Extension/UIImageView+.swift
@@ -17,7 +17,7 @@ extension UIImageView {
                             self?.image = image
                             self?.layer.cornerRadius = (self?.frame.size.width ?? 0) / 2.adjusted
                             self?.clipsToBounds = true
-                            self?.contentMode = .scaleAspectFit
+                            self?.contentMode = .scaleAspectFill
                         }
                     }
                 }
