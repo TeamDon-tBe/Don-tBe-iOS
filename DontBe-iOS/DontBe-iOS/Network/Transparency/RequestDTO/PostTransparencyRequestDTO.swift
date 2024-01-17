1aec25f2a8375504147aebdcca1f2d554c06c079
Sangwoo Byeon <tkddn0518@naver.com>
2024-01-18 04:15:11 +0900

    [Network] #91 - 투명도 적용 API RequestDTO 생성

---
+++ DontBe-iOS/DontBe-iOS/Network/Transparency/RequestDTO/PostTransparencyRequestDTO.swift
@@ -0,0 +1,16 @@
+//
+//  PostTransparencyRequestDTO.swift
+//  DontBe-iOS
+//
+//  Created by 변상우 on 1/18/24.
+//
+
+import Foundation
+
+// MARK: - PostTransparencyRequestDTO
+
+struct PostTransparencyRequestDTO: Encodable {
+    let alarmTriggerType: String
+    let targetMemberId: Int
+    let alarmTriggerId: Int
+}
