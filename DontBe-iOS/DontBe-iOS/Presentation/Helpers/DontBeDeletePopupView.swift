0ac17e373905fc27c178c2895078209f1e3d332c
yeonsu <drawingbonny@gmail.com>
2024-01-19 14:46:34 +0900

    [Feat] #119 - 삭제 팝업 뷰 생성

---
+++ DontBe-iOS/DontBe-iOS/Presentation/Helpers/DontBeDeletePopupView.swift
@@ -0,0 +1,68 @@
+//
+//  DontBeDeletePopupView.swift
+//  DontBe-iOS
+//
+//  Created by yeonsu on 1/19/24.
+//
+
+import UIKit
+
+import SnapKit
+
+final class DontBeDeletePopupView: UIView {
+
+    // MARK: - Properties
+    
+    // MARK: - UI Components
+    
+    let container: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 100, green: 100, blue: 100, alpha: 0.6)
+        view.layer.cornerRadius = 10.adjusted
+        return view
+    }()
+    
+    let toastLabel: UILabel = {
+        let label = UILabel()
+        label.text = StringLiterals.Toast.deleteText
+        label.textColor = .donBlack
+        label.textAlignment = .center
+        label.font = UIFont.font(.head3)
+        return label
+    }()
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setHierarchy()
+        setLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Extensions
+
+extension DontBeDeletePopupView {
+    
+    private func setHierarchy() {
+        self.addSubview(container)
+        container.addSubviews(toastLabel)
+    }
+    
+    private func setLayout() {
+        container.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        toastLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+    }
+}
