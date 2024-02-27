425ab4d44bc61acbd8640fe1b6248fbb28d5311a
Sangwoo Byeon <tkddn0518@naver.com>
2024-02-27 11:44:35 +0900

    [Feat] #150 - 회원가입 뷰 사진 앨범 권한 추가

--- DontBe-iOS/DontBe-iOS/Presentation/Join/ViewControllers/JoinProfileViewController.swift
+++ DontBe-iOS/DontBe-iOS/Presentation/Join/ViewControllers/JoinProfileViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Photos
+import PhotosUI
 
 import SnapKit
 
@@ -21,8 +23,12 @@ final class JoinProfileViewController: UIViewController {
         return self.originView.nickNameTextField.text ?? ""
     }.eraseToAnyPublisher()
     private lazy var finishButtonTapped = self.originView.finishActiveButton.publisher(for: .touchUpInside).map { _ in
-        return self.originView.nickNameTextField.text ?? ""
+        return EditUserProfileRequestDTO(nickname: self.originView.nickNameTextField.text ?? "",
+                                         is_alarm_allowed: true,
+                                         member_intro: "",
+                                         profile_image: self.originView.profileImage.image ?? ImageLiterals.Common.imgProfile)
     }.eraseToAnyPublisher()
+    
     
     // MARK: - UI Components
     
@@ -56,6 +62,7 @@ final class JoinProfileViewController: UIViewController {
         setUI()
         setHierarchy()
         setLayout()
+        setAddTarget()
         bindViewModel()
     }
     
@@ -91,6 +98,10 @@ extension JoinProfileViewController {
             $0.centerY.equalToSuperview()
             $0.leading.equalToSuperview().inset(23.adjusted)
         }
+    }
+    
+    private func setAddTarget() {
+        self.originView.plusButton.addTarget(self, action: #selector(plusButtonTapped), for: .touchUpInside)
     }
     
     private func setDelegate() {
@@ -137,5 +148,75 @@ extension JoinProfileViewController {
         self.originView.finishButton.isHidden = false
         self.originView.duplicationCheckDescription.text = StringLiterals.Join.duplicationCheckDescription
         self.originView.duplicationCheckDescription.textColor = .donGray8
+    }
+    
+    @objc
+    private func plusButtonTapped() {
+        let status = PHPhotoLibrary.authorizationStatus(for: .addOnly)
+        
+        switch status {
+        case .authorized, .limited:
+            presentPicker()
+        case .notDetermined:
+            PHPhotoLibrary.requestAuthorization(for: .addOnly) { [weak self] status in
+                DispatchQueue.main.async {
+                    if status == .authorized {
+                        self?.presentPicker()
+                    }
+                }
+            }
+        case .denied, .restricted:
+            authSettingOpen()
+        default:
+            break
+        }
+    }
+    
+    private func presentPicker() {
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images // 이미지만 필터링
+        configuration.selectionLimit = 1 // 선택 제한
+        
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func authSettingOpen() {
+        let message = "Don't Be 앱에 사진 권한이 없습니다.\n설정으로 이동하여 권한 설정을 해주세요."
+        
+        let alert = UIAlertController(title: "설정", message: message, preferredStyle: .alert)
+        
+        let cancle = UIAlertAction(title: "닫기", style: .default)
+        
+        let confirm = UIAlertAction(title: "권한설정하기", style: .default) { (UIAlertAction) in
+            UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+        }
+        
+        alert.addAction(cancle)
+        alert.addAction(confirm)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+}
+
+extension JoinProfileViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        dismiss(animated: true)
+        
+        guard let selectedImage = results.first else { return }
+        
+        selectedImage.itemProvider.loadObject(ofClass: UIImage.self) { (image, error) in
+            DispatchQueue.main.async {
+                if let image = image as? UIImage {
+                    self.originView.profileImage.image = image
+                    self.originView.profileImage.contentMode = .scaleAspectFill
+                    self.originView.profileImage.layer.cornerRadius = self.originView.profileImage.frame.size.width / 2
+                    self.originView.profileImage.clipsToBounds = true
+                } else if let error = error {
+                    print(error)
+                }
+            }
+        }
     }
 }
