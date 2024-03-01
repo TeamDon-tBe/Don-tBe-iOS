f68121c23761ca53bc17e59399fd504343b88f43
yeonsu <drawingbonny@gmail.com>
2024-03-02 00:56:43 +0900

    [Feat] #160 - AppDelegate에 window 추가

--- DontBe-iOS/DontBe-iOS/Application/AppDelegate.swift
+++ DontBe-iOS/DontBe-iOS/Application/AppDelegate.swift
@@ -12,7 +12,10 @@ import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    var window: UIWindow?
+    static var window: UIWindow { (UIApplication.shared.delegate?.window!)! }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         // 네비게이션 바 타이틀 텍스트 속성 설정
@@ -47,23 +50,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             KeychainWrapper.saveToken(refreshToken, forKey: "refreshToken")
         }
         
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    
 }
 
