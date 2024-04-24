7b2e94a99e2298e102ff16cf26ceef1232178323
yeonsu <drawingbonny@gmail.com>
2024-04-24 19:47:08 +0900

    [Feat] #181 - 앱 버전 확인 후 알림 띄우는 기능 구현

--- DontBe-iOS/DontBe-iOS/Application/SceneDelegate.swift
+++ DontBe-iOS/DontBe-iOS/Application/SceneDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 import KakaoSDKAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -34,6 +34,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 self.window?.rootViewController = navigationController
             }
             self.window?.makeKeyAndVisible()
+            self.checkAndUpdateIfNeeded()
         }
     }
     
@@ -44,35 +45,81 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             }
         }
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+        self.checkAndUpdateIfNeeded()
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
-}
-
+    
+    
+    func checkAndUpdateIfNeeded() {
+        AppStoreCheckManager().latestVersion { marketingVersion in
+            DispatchQueue.main.async {
+                guard let marketingVersion = marketingVersion else {
+                    print("앱스토어 버전을 찾지 못했습니다.")
+                    return
+                }
+                
+                let currentProjectVersion = AppStoreCheckManager.appVersion ?? ""
+                
+                let splitMarketingVersion = marketingVersion.split(separator: ".").map { $0 }
+                
+                let splitCurrentProjectVersion = currentProjectVersion.split(separator: ".").map { $0 }
+                
+                if splitCurrentProjectVersion.count > 0 && splitMarketingVersion.count > 0 {
+                
+                    if splitCurrentProjectVersion[0] < splitMarketingVersion[0] {
+                        self.showUpdateAlert(version: marketingVersion)
+                        
+                    } else if splitCurrentProjectVersion[1] < splitMarketingVersion[1] {
+                        self.showUpdateAlert(version: marketingVersion)
+                        
+                    } else {
+                        self.showUpdateAlert(version: marketingVersion)
+                    }
+                }
+            }
+        }
+    }
+    
+    func showUpdateAlert(version: String) {
+        let alert = UIAlertController(
+            title: "v 1.1.0 업데이트 안내\nDon't be가 업데이트 되었습니다.",
+            message: "•눌러서 바로 이동할 수 있는 링크를 삽입할 수 있어요.\n•내 글에 답글을 달거나 좋아요를 누른 상대의 프로필로 이동할 수 있어요.\n•그 외 자잘한 오류들을 해결했어요.",
+            preferredStyle: .alert
+        )
+        
+        let updateAction = UIAlertAction(title: "지금 업데이트", style: .default) { _ in
+            AppStoreCheckManager().openAppStore()
+        }
+        
+        let cancelAction = UIAlertAction(title: "나중에", style: .destructive, handler: nil)
+        
+        [ cancelAction, updateAction ].forEach { alert.addAction($0) }
+        window?.rootViewController?.present(alert, animated: true, completion: nil)
+    }
+    }
--- DontBe-iOS/DontBe-iOS/Global/Shared/AppStoreCheckManager.swift
+++ DontBe-iOS/DontBe-iOS/Global/Shared/AppStoreCheckManager.swift
@@ -10,23 +10,29 @@ import UIKit
 class AppStoreCheckManager {
     
     static let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
-    
     static let buildNumber = Bundle.main.infoDictionary?["CFBundleVersion"] as? String
     static let appStoreOpenUrlString = "itms-apps://itunes.apple.com/app/apple-store/6475622329"
     
-    func latestVersion() -> String? {
+    func latestVersion(completion: @escaping (String?) -> Void) {
         let appleID = "6475622329"
-        guard let url = URL(string: "https://itunes.apple.com/lookup?id=\(6475622329)&country=kr"),
-              let data = try? Data(contentsOf: url),
-              let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any],
-              let results = json["results"] as? [[String: Any]],
-              let appStoreVersion = results[0]["version"] as? String else {
-            return nil
+        guard let url = URL(string: "https://itunes.apple.com/lookup?id=\(appleID)&country=kr") else {
+            completion(nil)
+            return
         }
-        return appStoreVersion
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data, error == nil,
+                  let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any],
+                  let results = json["results"] as? [[String: Any]],
+                  let appStoreVersion = results[0]["version"] as? String else {
+                completion(nil)
+                return
+            }
+            
+            completion(appStoreVersion)
+        }.resume()
     }
     
-    // 앱 스토어로 이동 -> urlStr 에 appStoreOpenUrlString 넣으면 이동
     func openAppStore() {
         guard let url = URL(string: AppStoreCheckManager.appStoreOpenUrlString) else { return }
         if UIApplication.shared.canOpenURL(url) {
